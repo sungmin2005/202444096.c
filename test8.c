061603560bed7fdb5b7492a22d85640bb5460741
sungmin2005 <163946877+sungmin2005@users.noreply.github.com>
2024-03-27 23:08:20 +0900

    Add files via upload

---
+++ test8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float su;
+
+    printf("소수 여섯째 자리까지 실수 입력 : ");
+    scanf("      ", &su);
+
+    printf("소수 둘째 자리까지 출력 :        \n",su);
+    printf("전체 자릿수 6자리, 소수 첫쨰 자리까지 출력 :      \n", su);
+
+    return 0;
+}
